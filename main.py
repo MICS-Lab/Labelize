69b8b374349baff2efc931819f009fba1f1b893a
pauldubois98 <pauldubois98@gmail.com>
2022-12-28 22:21:26 +0100

    added previous button

--- main.py
+++ main.py
@@ -14,12 +14,25 @@ images = os.listdir(directory)
 def next_image():
   global counter
   global images
-  global size
   global window
   global label
   counter += 1
   if counter >= len(images):
     counter = 0
+  print(counter)
+
+  img = PhotoImage(file=os.path.join(directory, images[counter]))
+  label.configure(image=img)
+  label.image = img# Function to display the next image
+
+def prev_image():
+  global counter
+  global images
+  global window
+  global label
+  counter -= 1
+  if counter <0:
+    counter = len(images) - 1
   print(counter)
 
   img = PhotoImage(file=os.path.join(directory, images[counter]))
@@ -30,15 +43,18 @@ def next_image():
 def classify_image(classification):
   global counter
   global images
-  global size
   global window
   global label
   global csv
   csv += f"{images[counter]}, {classification}\n"
   next_image()
 
-button = tk.Button(window, text='Next', command=next_image)
-button.pack()
+top_btns = tk.Frame(window)
+button = tk.Button(top_btns, text='Next', command=next_image)
+button.grid(column=0, row=0)
+button = tk.Button(top_btns, text='Previous', command=prev_image)
+button.grid(column=1, row=0)
+top_btns.pack()
 
 classes_btns = tk.Frame(window)
 button0 = tk.Button(classes_btns, text='Glasses', command=lambda :classify_image("glasses"))
