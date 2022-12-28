af54b71105a5544181393e89e990e178894c13d6
pauldubois98 <pauldubois98@gmail.com>
2022-12-28 19:01:51 +0100

    next button

--- main.py
+++ main.py
@@ -11,9 +11,40 @@ directory = filedialog.askdirectory()
 # Get a list of all the images in the directory
 images = os.listdir(directory)
 
-# Iterate through the images and display them one by one
-for image in images:
-  img = PhotoImage(file=os.path.join(directory, image))
-  label = tk.Label(window, image=img)
-  label.pack()
-  window.mainloop()
+# Set the size to which you want to resize the images
+size = (200, 200)
+
+# Create a counter to keep track of the current image
+counter = 0
+
+# Function to display the next image
+def next_image():
+  global counter
+  global images
+  global size
+  global window
+  global label
+  # Increment the counter
+  counter += 1
+  # Check if the counter has reached the end of the list
+  if counter >= len(images):
+    # Reset the counter
+    counter = 0
+  print(counter)
+
+  img = PhotoImage(file=os.path.join(directory, images[counter]))
+  label.configure(image=img)
+  label.image = img
+
+
+# Create a button to display the next image
+button = tk.Button(window, text='Next', command=next_image)
+button.pack()
+
+
+img = PhotoImage(file=os.path.join(directory, images[0]))
+label = tk.Label(window, image=img)
+label.pack()
+
+# Run the Tkinter event loop
+window.mainloop()
