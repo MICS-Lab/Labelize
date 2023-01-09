31b39294859072755f94574cf02d8160cfb8c21f
pauldubois98 <pauldubois98@gmail.com>
2023-01-09 14:56:44 +0100

    automatic creation of buttons

--- main.py
+++ main.py
@@ -72,14 +72,9 @@ button.grid(column=1, row=0)
 top_btns.pack()
 
 classes_btns = tk.Frame(window)
-button0 = tk.Button(classes_btns, text='Glasses', command=lambda :classify_image("glasses"))
-button0.grid(column=0, row=0)
-button1 = tk.Button(classes_btns, text='No Glasses', command=lambda :classify_image("no glasses"))
-button1.grid(column=1, row=0)
-button1 = tk.Button(classes_btns, text='Unclear', command=lambda :classify_image("unclear"))
-button1.grid(column=2, row=0)
-button1 = tk.Button(classes_btns, text='Bad Image', command=lambda :classify_image("bad image"))
-button1.grid(column=3, row=0)
+for i,c in enumerate(params['classes']):
+    bt = tk.Button(classes_btns, text=c, command=lambda :classify_image(c))
+    bt.grid(column=i, row=0)
 
 img = PhotoImage(file=os.path.join(directory, images[0]))
 label = tk.Label(window, image=img)
